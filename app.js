c5bc3e8ebb685e1a964c198303db540e474fb8aa
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2018-04-18 10:31:45 +0430

    add bot buttons

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const TeleBot = require('telebot');
-
 const bot = new TeleBot('583347744:AAGr1XHfoAm0E7OWxiVXEI0nOARmKqwSuhY');
 
-bot.on('text', (msg) => msg.reply.text('salam behx'));
+bot.on(['/start', 'back'], msg => {
+    let replyMarkup = bot.keyboard([
+	[bot.button('/newMood'), bot.button('/sendRecommendation')]
+    ], {resize: true});
+    return bot.sendMessage(msg.from.id, 'در حال پردازش', {replyMarkup});
+});
+
+
+bot.on(['/newRecommendation'], msg => {
+    bot.on('text', msg => {
+        console.log(msg);
+    })
+});
+
+
+bot.on(['/searchRecommendation'], msg => {
+    bot.on('text', msg => {
+        console.log(msg);
+    })
+});
 
 bot.start();
-
