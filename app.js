ad338bb9c275af0e8bcd49c3e466b056a5626d60
Amir <aseyhani@icloud.com>
2018-04-18 11:40:19 +0430

    add recom schema

--- app.js
+++ app.js
@@ -4,23 +4,45 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/itlab");
 mongoose.Promise = global.Promise;
 
+let controller = require('./controller');
+
 bot.on(['/start', 'back'], msg => {
     let replyMarkup = bot.keyboard([
-	[bot.button('/newMood'), bot.button('/sendRecommendation')]
+	[bot.button('/newRecom'), bot.button('/sendRecommendation')]
     ], {resize: true});
     return bot.sendMessage(msg.from.id, 'در حال پردازش', {replyMarkup});
 });
 
-bot.on(['/newRecommendation'], msg => {
-    bot.on('text', msg => {
-        console.log(msg);
-    })
+bot.on(['/newRecom'], msg => {
+    bot.sendMessage(msg.from.id, 'Enter tag').then(() => {
+      bot.event('/getTag', msg);
+    });
+});
+
+const moodCreate = require('./moodCreate');
+let recom = {
+}
+bot.on(['/getTag'], msg => {
+  bot.on('text', (text) => {
+    recom.tags = text.text;
+    bot.sendMessage(msg.from.id, 'Enter recom').then(() => {
+      bot.event('/getRecom', msg)
+  });
+  });
+});
+
+bot.on(['/getRecom'], msg => {
+  bot.on('text', (text) => {
+    recom.recom = text.text;
+    bot.sendMessage(msg.from.id, 'Created');
+  })
 });
 
 bot.on(['/searchRecommendation'], msg => {
     bot.on('text', msg => {
-        console.log(msg);
+
     })
 });
+
 
 bot.start();
---
+++ controller.js
@@ -0,0 +1,17 @@
+const Recom = require('./recommendation');
+
+module.exports = {
+  find: (tags) =>{
+    return Recom.find({tags:{$all:tags}});
+  },
+  create: (recom, tags) =>{
+    return new Promise((resolve,reject) => {
+      Recom.create({recom, tags}, (err, newRecom) => {
+        if(err)
+          reject(err);
+        else
+          resolve(newRecom);
+      });
+    });
+  }
+};
