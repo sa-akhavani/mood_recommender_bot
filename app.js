10e2d34592b32a515c1afa897cf80dac97cafb67
Amir <aseyhani@icloud.com>
2018-04-18 09:44:16 +0430

    add recom schema

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+let express = require('express');
+const app = express();
+let mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/itlab");
+mongoose.Promise = global.Promise;
+
 const TeleBot = require('telebot');
 
 const bot = new TeleBot('583347744:AAGr1XHfoAm0E7OWxiVXEI0nOARmKqwSuhY');
 
-bot.on('text', (msg) => msg.reply.text('salam behx'));
+bot.on('text', (msg) => msg.reply.text('Bye behx'));
 
 bot.start();
 
+module.exports = app;
